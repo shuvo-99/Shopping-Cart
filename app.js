5ba97254a162550164dd52b5d3397f06238afa0b
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-13 12:26:19 +0600

    phone-no. + price update

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-function updateCaseNumber(caseNum) {
-  const caseInput = document.getElementById("case-number");
-  let caseNumber = caseInput.value;
-  if (caseNum == "case-plus") {
-    caseNumber = parseFloat(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseNumber = parseFloat(caseNumber) - 1;
+function updateProductNumber(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + "-number");
+  let productNumber = productInput.value;
+  if (isIncreasing == true) {
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
-  const caseTotal = document.getElementById("case-total");
-  caseTotal.innerText = caseNumber * 59;
+  productInput.value = productNumber;
+
+  // update total
+  const productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
 }
 
+// phone increasing decreasing events
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateProductNumber("phone", 1219, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateProductNumber("phone", 1219, false);
+});
+
+// case increasing decreasing events
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber("case-plus");
+  updateProductNumber("case", 59, true);
 });
 
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber("case-minus");
+  updateProductNumber("case", 59, false);
 });
