135d8a26a47c059b27e8f813628a83a7ef73f01d
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-13 11:44:01 +0600

    case-no. + price update

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('I am in a separate file')
+function updateCaseNumber(caseNum) {
+  const caseInput = document.getElementById("case-number");
+  let caseNumber = caseInput.value;
+  if (caseNum == "case-plus") {
+    caseNumber = parseFloat(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseNumber = parseFloat(caseNumber) - 1;
+  }
+  caseInput.value = caseNumber;
+  const caseTotal = document.getElementById("case-total");
+  caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber("case-plus");
+});
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber("case-minus");
+});
